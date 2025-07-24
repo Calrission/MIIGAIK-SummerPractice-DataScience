7efe4d2a61bd4f7b0e017561f463deff6ff9ccad
artemii <kavrese@gmail.com>
2025-07-24 17:27:22 +0300

    init streamlit

--- practices/practice_1/theory_3.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('assets/sample_1.html', 'r') as file:
-    content = file.read()
-    soup = BeautifulSoup(content, "html.parser")
-
-for child in soup.descendants:
-    if child.name:
-        print(child.name)
